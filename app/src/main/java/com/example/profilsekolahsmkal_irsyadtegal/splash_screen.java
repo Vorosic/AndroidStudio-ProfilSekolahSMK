cc208981d36d0d6e5c850dd51076d32de91718a6
Zaky <donquixotevorosic@gmail.com>
2024-03-27 00:38:57 +0700

    initial commit

---
+++ app/src/main/java/com/example/profilsekolahsmkal_irsyadtegal/splash_screen.java
@@ -0,0 +1,35 @@
+package com.example.profilsekolahsmkal_irsyadtegal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent pindah = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(pindah);
+                finish();
+            }
+        }, 3000);
+    }
+}
