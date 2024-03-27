587eaf2fa9bb31868aca1df2d40a406de8ed8e64
Zaky <donquixotevorosic@gmail.com>
2024-03-27 13:19:38 +0700

    initial commit

--- app/src/main/java/com/example/profilsekolahsmkal_irsyadtegal/landingpage.java
+++ app/src/main/java/com/example/profilsekolahsmkal_irsyadtegal/landingpage.java
@@ -2,7 +2,8 @@ package com.example.profilsekolahsmkal_irsyadtegal;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class splash_screen extends AppCompatActivity {
+public class landingpage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_landingpage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        new Handler().postDelayed(new Runnable(){
+        Button masuk = findViewById(R.id.masuk);
+
+        masuk.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void run() {
-                Intent pindah = new Intent(splash_screen.this, landingpage.class);
-                startActivity(pindah);
+            public void onClick(View v) {
+                startActivity(new Intent(landingpage.this, MainActivity.class));
                 finish();
             }
-        }, 3000);
+        });
     }
 }
